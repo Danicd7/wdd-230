1f6f6b810bde9a6f8435a1abafcf44409d936346
Danicd7 <danielsilva_88@outlook.com>
2024-02-08 15:41:21 +0100

    commit

---
+++ week03/scripts/bom3.js
@@ -0,0 +1,60 @@
+//Year actualization 
+const date = new Date();
+const year = date.getFullYear();
+document.querySelector("#currentyear").textContent = year;
+
+
+document.querySelector(
+	"#lastModified"
+).textContent = `Last Update: ${document.lastModified}`;
+
+
+//the bom script
+let chaptersArray = getChapterList() || [];
+
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+  });
+
+  button.addEventListener('click', function() {
+    if (input.value != '') {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        ChapterList();
+        input.value = '';
+        input.focus();
+    }
+});
+
+function displayList(item) {
+    let li = document.createElement('li');
+    let deletebutton = document.createElement('button');
+    li.textContent = item; 
+    deletebutton.textContent = '❌';
+    deletebutton.classList.add('delete'); 
+    li.append(deletebutton);
+    list.append(li);
+    deletebutton.addEventListener('click', function () {
+      list.removeChild(li);
+      deleteChapter(li.textContent); 
+      input.focus();
+    });
+  }
+
+  function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+  }
+
+  function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+  }
+
+  function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+  }
