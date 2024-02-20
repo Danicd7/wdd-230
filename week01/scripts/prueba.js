bfa9339bf73b4fba5e6e157b5f8e915041a872dd
Danicd7 <danielsilva_88@outlook.com>
2024-02-20 15:49:32 +0100

    commit

---
+++ week01/scripts/prueba.js
@@ -0,0 +1,53 @@
+const membersURL       = "https://danicd7.github.io/wdd-230/week01/data/members.json";
+const memberGridEl     = document.getElementById("member-grid");
+const memberGridToggle = document.getElementById("member-grid-toggle");
+
+async function fetchMembers() {
+  try {
+    const response = await fetch(membersURL);
+
+    if (!response.ok) {
+      throw new Error(await response.text());
+    }
+
+    const data = await response.json();
+
+    displayMembers(data.members);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const displayMembers = (members) => {
+  let memberHTML = members.map(member => {
+    return `<div class="card member">
+              <div class="member-card-header">
+                <h3>${member.name}</h3>
+                <img src="./images/${member.image}" width="50" height="50">
+              </div>
+              <p>Address: ${member.address}</p>
+              <p>Phone: ${member.phone}</p>
+              <p>Membership level: ${member.membership_level}</p>
+              <p class="other-info">Other Info: ${member.other_information}</p>
+              <a href="https://google.com" target="_blank">Website</a>
+            </div>`
+  }).join("");
+
+  memberGridEl.innerHTML = memberHTML;
+}
+
+memberGridToggle.addEventListener("click", () => {
+  memberGridEl.classList.toggle("member-grid");
+  memberGridEl.classList.toggle("member-column");
+
+  let members = document.getElementsByClassName("member");
+
+  Array.from(members).forEach(memberEl => {
+    memberEl.classList.toggle("card");
+    memberEl.classList.toggle("member-line-item");
+    memberEl.querySelector("img").classList.toggle("hidden");
+  });
+});
+
+// Main
+fetchMembers();
